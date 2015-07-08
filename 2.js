bf240555663d715a2cd42a074f997721bfc91a8b
ShaoLing <shaoling@beisen.com>
2015-07-08 15:47:13 +0800

    D

--- 2.js
+++ 2.js
@@ -1,9 +1,4 @@
-// **This example illustrates the binding of DOM events to View methods.**
-//
-// _Working example: [2.html](../2.html)._
-// _[Go to Example 3](3.html)_
 
-//
 (function($){
   var ListView = Backbone.View.extend({
     el: $('body'), // el attaches to existing element
@@ -32,17 +27,3 @@
 
   var listView = new ListView();
 })(jQuery);
-
-// <div style="float:left; margin-bottom:40px;"><img style="width:36px; margin:5px 10px 0 5px;" src="https://g.twimg.com/Twitter_logo_blue.png"/></div> <div style="background:rgb(245,245,255); padding:10px;">Follow me on Twitter: <a target="_blank" href="http://twitter.com/r2r">@r2r</a> </div>
-// <script>
-//   if (window.location.href.search(/\?x/) < 0) {
-//     var _gaq = _gaq || [];
-//     _gaq.push(['_setAccount', 'UA-924459-7']);
-//     _gaq.push(['_trackPageview']);
-//     (function() {
-//       var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-//       ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-//       var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-//     })();
-//   }
-// </script>
